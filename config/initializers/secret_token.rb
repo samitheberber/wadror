2c424d7cb2d1c409ce0636baaa7e693ce1057e2b
Sami Saada <sami.saada@gmail.com>
2013-08-28 23:46:06 +0300

    rails structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RateBeer::Application.config.secret_key_base = '2a377b7590ab7839adbfe3960cdc8d5dc1426beb46dd67e874e2cc6223844300012f7c13e0c2b151f5118e8370b4becd250fcf96332013e4996596ee20f07f1c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RateBeer::Application.config.session_store :cookie_store, key: '_ratebeer_session'
