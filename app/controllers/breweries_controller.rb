fa04d81570677b223350005a896123b7ea91fb2d
Sami Saada <sami.saada@gmail.com>
2013-09-03 02:23:11 +0300

    auth from list

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -65,8 +65,9 @@ class BreweriesController < ApplicationController
 
   private
     def authenticate
+      admin_accounts = { "admin" => "secret", "pekka" => "beer", "arto" => "foobar", "matti" => "ittam"}
       authenticate_or_request_with_http_basic do |username, password|
-        username == "admin" and password == "secret"
+        admin_accounts[username] and admin_accounts[username] == password
       end
     end
 
