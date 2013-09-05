b258c08b3db5103a243c334ba30270d592561e75
Sami Saada <sami.saada@gmail.com>
2013-09-06 02:15:45 +0300

    user ratings

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,5 +1,6 @@
 class Rating < ActiveRecord::Base
   belongs_to :beer
+  belongs_to :user   # rating kuuluu myös käyttäjään
 
   def to_s
     "#{beer.name} #{score}"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ActiveRecord::Base
+  has_many :ratings   # käyttäjällä on monta ratingia
 end
