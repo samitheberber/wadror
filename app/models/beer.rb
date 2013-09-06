83e5d179d496c9ac01a4ab9f4982214d4fdf5491
Sami Saada <sami.saada@gmail.com>
2013-09-06 03:19:54 +0300

    raters

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,7 +3,7 @@ class Beer < ActiveRecord::Base
 
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
-  has_many :users, through: :ratings
+  has_many :raters, through: :ratings, source: :user
 
   def to_s
     "#{name}, #{brewery.name}"
