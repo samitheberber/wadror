dd5a7a1d5f43e56885eaeeedfde38c9856bdc29f
Sami Saada <sami.saada@gmail.com>
2013-09-09 02:34:48 +0300

    users integration

---
+++ spec/features/users_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe "User" do
+  before :each do
+    FactoryGirl.create :user
+  end
+
+  describe "who has signed up" do
+    it "can sign in with right credentials" do
+      visit signin_path
+      fill_in('username', with: 'Pekka')
+      fill_in('password', with: 'foobar1')
+      click_button('Log in')
+
+      expect(page).to have_content 'Welcome back!'
+      expect(page).to have_content 'Pekka'
+    end
+
+    it "is redirected back to sign in form if wrong credentials given" do
+      visit signin_path
+      fill_in('username', with: 'Pekka')
+      fill_in('password', with: 'wrong')
+      click_button('Log in')
+
+      expect(current_path).to eq(signin_path)
+      expect(page).to have_content 'username and password do not match'
+    end
+  end
+
+  it "when signed up with good credentials, is added to the system" do
+    visit signup_path
+    fill_in('user_username', with: 'Brian')
+    fill_in('user_password', with: 'secret55')
+    fill_in('user_password_confirmation', with: 'secret55')
+
+    expect{
+      click_button('Create User')
+    }.to change{User.count}.by(1)
+  end
+end
