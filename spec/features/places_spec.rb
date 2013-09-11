e801e286877b38695d2abb93c8ab08080bac3e5f
Sami Saada <sami.saada@gmail.com>
2013-09-11 15:57:25 +0300

    integration test for places

---
+++ spec/features/places_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "Places" do
+  it "if one is returned by the API, it is shown at the page" do
+    BeermappingAPI.stub(:places_in).with("kumpula").and_return( [  Place.new(name: "Oljenkorsi") ] )
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+
+    expect(page).to have_content "Oljenkorsi"
+  end
+end
