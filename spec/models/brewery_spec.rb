c4bc0ee553fdfb04dc4c19bbd81801357e01a0ba
Sami Saada <sami.saada@gmail.com>
2013-09-11 11:55:26 +0300

    subject style

--- spec/models/brewery_spec.rb
+++ spec/models/brewery_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe Brewery do
-  it "has the name and year set correctly and is saved to database" do
-    brewery = Brewery.create name: "Schlenkerla", year: 1674
+  describe "when initialized with name Schlenkerla and year 1674" do
+    subject{ Brewery.create name: "Schlenkerla", year: 1674 }
 
-    expect(brewery.name).to eq("Schlenkerla")
-    expect(brewery.year).to eq(1674)
-    expect(brewery).to be_valid
+    it { should be_valid }
+    its(:name) { should eq("Schlenkerla") }
+    its(:year) { should eq(1674) }
   end
 
   it "without a name is not valid" do
