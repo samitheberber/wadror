39cb82929e471a24b06ae72fe29d15380fc425a2
Sami Saada <sami.saada@gmail.com>
2013-09-18 07:05:44 +0300

    indexing usernames

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130918032823) do
+ActiveRecord::Schema.define(version: 20130918040429) do
 
   create_table "beer_clubs", force: true do |t|
     t.string   "name"
@@ -78,5 +78,7 @@ ActiveRecord::Schema.define(version: 20130918032823) do
     t.string   "password_digest"
     t.boolean  "admin",           default: false
   end
+
+  add_index "users", ["username"], name: "index_users_on_username"
 
 end
