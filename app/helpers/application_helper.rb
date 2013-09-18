393114fae5677754c5cc4bbb5086e96c8a62a0fb
Sami Saada <sami.saada@gmail.com>
2013-09-18 06:00:28 +0300

    button helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def edit_and_destroy_buttons(item)
+    if current_user
+      edit = link_to('Edit', url_for([:edit, item]), class: "btn btn-primary")
+      if current_user.admin
+        del = link_to('Destroy', item, method: :delete,
+                      data: {confirm: 'Are you sure?' }, class: "btn btn-danger")
+      end
+      raw("#{edit} #{del}")
+    end
+  end
 end
